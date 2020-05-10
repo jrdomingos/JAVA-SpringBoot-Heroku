247cbb1f79f173cec3192533ceabb9021b04e1c9
Jr Domingos <domingoshot@hotmail.com>
2020-05-10 12:28:09 -0300

    Update Application.java

--- back-end/src/main/java/com/guesser/spring/Application.java
+++ back-end/src/main/java/com/guesser/spring/Application.java
@@ -1,5 +1,7 @@
 package com.guesser.spring;
 
+                          /* ***********  @Author : JrD - Junior Domingos *********** */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
